dc0c721a54dd299dedf9c87bdeda662435eb918c
Khailica <yujinn@live.ru>
2022-12-16 22:52:20 +0700

    add subtast for homework4

--- Sber - Data_Engineer/Module_Prof/homework_4/homework4.sql
+++ Sber - Data_Engineer/Module_Prof/homework_4/homework4.sql
@@ -9,51 +9,20 @@
 */
 
 
-SELECT
-     *
-FROM
-     de11tm.hw4_fct_transaction
-WHERE
-     client_id = 37646 AND
-     trans_dt = '2021-06-30'
-ORDER BY
-     client_id
-,    trans_dt DESC;
-
-
-SELECT
-     client_id
-,    date_part('quarter', hft.trans_dt) quarter
-,    max(hft.trans_dt)
-FROM
-     de11tm.hw4_fct_transaction hft
-WHERE
-     hft.client_id = 37646
-GROUP BY
-     hft.client_id
-,    date_part('quarter', hft.trans_dt)
-ORDER BY
-     hft.client_id
-,    date_part('quarter', hft.trans_dt)
-
-
-
---EXPLAIN 
 SELECT DISTINCT
      client_id
-,    date_part('quarter', hft.trans_dt) AS quarter
-,    last_value(hft.trans_amt) OVER(PARTITION BY hft.client_id, date_part('quarter', hft.trans_dt)
-          ORDER BY hft.client_id, date_part('quarter', hft.trans_dt)) AS sum_last_day_trans
-,    sum(hft.trans_amt) OVER w AS trans_amt
-,    max(hft.trans_amt) OVER w AS trans_max
-,    round(avg(hft.trans_amt) OVER w, 0) AS trans_avg
+,    date_part('quarter', trans_dt) AS "quarter"
+,    last_value(trans_amt) OVER(PARTITION BY client_id, date_part('quarter', trans_dt) ORDER BY client_id, date_part('quarter', trans_dt)) AS sum_last_day_trans
+,    sum(trans_amt) OVER w AS trans_amt
+,    max(trans_amt) OVER w AS trans_max
+,    round(avg(trans_amt) OVER w, 0) AS trans_avg
 FROM
-     de11tm.hw4_fct_transaction hft
+     de11tm.hw4_fct_transaction
 WINDOW
-     w AS(PARTITION BY hft.client_id, date_part('quarter', hft.trans_dt))
+     w AS(PARTITION BY client_id, date_part('quarter', trans_dt))
 ORDER BY
      client_id
-,    date_part('quarter', hft.trans_dt);
+,    date_part('quarter', trans_dt);
 
      
      
@@ -68,14 +37,31 @@ ORDER BY
 */
   	 
 
+WITH cte AS (
+     SELECT
+          client_id
+     ,    date_part('month', trans_dt) AS current_month
+     ,    sum(trans_amt) AS sum_trans
+     FROM
+          de11tm.hw4_fct_transaction
+     GROUP BY
+          client_id
+     ,    date_part('month', trans_dt)
+)
 SELECT
-     client_id
-,    trans_dt
-,    trans_amt
+     *
+,    lag(sum_trans) OVER(PARTITION BY client_id ORDER BY current_month) AS sum_trans_1preceding_month
+,    lead(sum_trans) OVER(PARTITION BY client_id ORDER BY current_month) AS sum_trans_1following_month
+,    sum(sum_trans) OVER(PARTITION BY client_id ORDER BY current_month) AS sum_trans_additive
+,    sum(sum_trans) OVER(PARTITION BY client_id ORDER BY current_month ROWS BETWEEN 2 PRECEDING AND 1 FOLLOWING) AS sum_trans_sliding_window
 FROM
-     de11tm.hw4_fct_transaction hft; 
+     cte
+ORDER BY
+     client_id
+,    current_month;
  	 
   	  
+
 
 /*
 3* На основе таблицы de11tm.HW4_FCT_TRANSACTION для каждого клиента
@@ -83,16 +69,43 @@ FROM
 (Если по клиенту несколько транзакций прошло в один день, то выведите их все).
 */
  	
-
+WITH cte1 AS (
+     SELECT
+          client_id
+     ,    date_part('month', trans_dt) AS "month"
+     ,    trans_dt
+     ,    dense_rank() OVER(PARTITION BY client_id, date_part('month', trans_dt) ORDER BY trans_dt) AS grade
+     ,    trans_amt
+     FROM
+          de11tm.hw4_fct_transaction
+     ORDER BY
+          client_id
+     ,    date_part('month', trans_dt)
+)
+, cte2 AS (
+     SELECT
+          client_id
+     ,    month
+     ,    max(grade) - 1 AS penultimate_grade
+     FROM
+          cte1
+     GROUP BY
+          client_id
+     ,    month
+     ORDER BY
+          client_id
+     ,    month
+)
 SELECT
-     client_id
-,    trans_dt
-,    trans_amt
+     cte1.client_id
+,    cte1."month"
+,    cte1.trans_dt
+,    cte1.trans_amt
 FROM
-     de11tm.hw4_fct_transaction hft; 
-
-
- 
-
-
+     cte2
+JOIN
+     cte1
+          ON cte1.client_id = cte2.client_id AND
+          cte1."month" = cte2."month" AND
+          cte1.grade = cte2.penultimate_grade;
 	 
