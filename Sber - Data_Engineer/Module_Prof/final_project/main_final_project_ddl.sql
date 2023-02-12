cf56dfdde710c16a5fff185913f6b963e3de53c6
Khailica <yujinn@live.ru>
2023-02-12 23:41:07 +0700

    update_modules

--- Sber - Data_Engineer/Module_Prof/final_project/main_final_project_ddl.sql
+++ Sber - Data_Engineer/Module_Prof/final_project/main_final_project_ddl.sql
@@ -1,4 +1,4 @@
--- Создаём таблицы измерений в приёмнике:
+-- 1.Создаём таблицы измерений в приёмнике:
 
 DROP TABLE IF EXISTS de11tm.ykir_dwh_dim_accounts_hist;
 CREATE TABLE IF NOT EXISTS de11tm.ykir_dwh_dim_accounts_hist(
@@ -87,7 +87,7 @@ CREATE TABLE IF NOT EXISTS de11tm.ykir_dwh_dim_terminals_hist(
 
 
 
--- Создаём таблицы фактов в приёмнике:
+-- 2.Создаём таблицы фактов в приёмнике:
 
 DROP TABLE IF EXISTS de11tm.ykir_dwh_fact_passport_blacklist;
 CREATE TABLE IF NOT EXISTS de11tm.ykir_dwh_fact_passport_blacklist(
@@ -98,31 +98,28 @@ CREATE TABLE IF NOT EXISTS de11tm.ykir_dwh_fact_passport_blacklist(
 );
 
 
-
 DROP TABLE IF EXISTS de11tm.ykir_dwh_fact_transactions;
 CREATE TABLE IF NOT EXISTS de11tm.ykir_dwh_fact_transactions(
-     trans_id varchar(20)
-,    trans_date timestamp(0)
-,    card_num varchar(20)
-,    oper_type varchar(7)
-,    amt numeric(10,2)
-,    oper_result varchar(10)
-,    terminal varchar(10)
+     trans_id
+          varchar(20)
+,    trans_date
+          timestamp(0)
+,    card_num
+          varchar(20)
+,    oper_type
+          varchar(7)
+,    amt
+          numeric(10,2)
+,    oper_result
+          varchar(10)
+,    terminal
+          varchar(10)
 );
 
+       
 
-     account_num
-          varchar(20)
-,    valid_to
-          date
-,    client
-          varchar(10)
-,    effective_from
-          timestamp(0)
-
-          
-          
--- Создаём стейджинговые таблицы:
+     
+-- 3.Создаём стейджинговые таблицы:
           
 DROP TABLE IF EXISTS de11tm.ykir_stg_dim_accounts;
 CREATE TABLE IF NOT EXISTS de11tm.ykir_stg_dim_accounts(
@@ -179,14 +176,12 @@ CREATE TABLE IF NOT EXISTS de11tm.ykir_stg_dim_clients(
 
 DROP TABLE IF EXISTS de11tm.ykir_stg_fact_passport_blacklist;
 CREATE TABLE IF NOT EXISTS de11tm.ykir_stg_fact_passport_blacklist(
-     passport_num
-          varchar(15)
-,    entry_dt
+     "date"
           date
-,    create_dt -- возможно лишняя запись
-          timestamp(0)
-,    update_dt -- возможно лишняя запись
-          timestamp(0)
+,    passport
+          varchar(15)
+,    date_file
+          date
 );
 
 
@@ -200,24 +195,129 @@ CREATE TABLE IF NOT EXISTS de11tm.ykir_stg_dim_terminals(
           varchar(30)
 ,    terminal_address
           varchar(100)
-,    create_dt -- возможно лишняя запись
-          timestamp(0)
-,    update_dt
-          timestamp(0)
+,    date_file
+          date
 );
 
 
 DROP TABLE IF EXISTS de11tm.ykir_stg_fact_transactions;
 CREATE TABLE IF NOT EXISTS de11tm.ykir_stg_fact_transactions(
-     transaction_id varchar(20)
-,    transaction_date timestamp(0)
-,    amount numeric(10,2) -- возможно тип надо varchar(15)
-,    card_num varchar(20)
-,    oper_type varchar(7)
-,    oper_result varchar(10)
-,    terminal varchar(10)
-,    create_dt -- возможно лишняя запись
+     transaction_id
+          varchar(20)
+,    transaction_date
           timestamp(0)
-,    update_dt -- возможно лишняя запись
+,    amount
+          numeric(10,2)
+,    card_num
+          varchar(20)
+,    oper_type
+          varchar(7)
+,    oper_result
+          varchar(10)
+,    terminal
+          varchar(10)
+,    date_file
+          date
+);
+
+
+
+-- 4. Создаем таблицы метаданных:
+DROP TABLE IF EXISTS de11tm.ykir_meta_dim_accounts;
+CREATE TABLE IF NOT EXISTS de11tm.ykir_meta_dim_accounts(
+     max_update_dt
           timestamp(0)
+);
+
+DROP TABLE IF EXISTS de11tm.ykir_meta_dim_cards;
+CREATE TABLE IF NOT EXISTS de11tm.ykir_meta_dim_cards(
+     max_update_dt
+          timestamp(0)
+);
+
+DROP TABLE IF EXISTS de11tm.ykir_meta_dim_clients;
+CREATE TABLE IF NOT EXISTS de11tm.ykir_meta_dim_clients(
+     max_update_dt
+          timestamp(0)
+);
+
+DROP TABLE IF EXISTS de11tm.ykir_meta_dim_clients;
+CREATE TABLE IF NOT EXISTS de11tm.ykir_meta_dim_clients(
+     max_update_dt
+          timestamp(0)
+);
+
+DROP TABLE IF EXISTS de11tm.ykir_meta_dim_terminals;
+CREATE TABLE IF NOT EXISTS de11tm.ykir_meta_dim_terminals(
+     max_update_dt
+          timestamp(0)
+);
+
+DROP TABLE IF EXISTS de11tm.ykir_meta_fact_passport_blacklist;
+CREATE TABLE IF NOT EXISTS de11tm.ykir_meta_fact_passport_blacklist(
+     max_update_dt
+          timestamp(0)
+);
+
+DROP TABLE IF EXISTS de11tm.ykir_meta_fact_transactions;
+CREATE TABLE IF NOT EXISTS de11tm.ykir_meta_fact_transactions(
+     max_update_dt
+          timestamp(0)
+);
+
+
+
+DROP TABLE IF EXISTS de11tm.ykir_stg_delete_dim_accounts;
+CREATE TABLE IF NOT EXISTS de11tm.ykir_stg_delete_dim_accounts(
+     max_update_dt
+          timestamp(0)
+);
+
+DROP TABLE IF EXISTS de11tm.ykir_stg_delete_dim_cards;
+CREATE TABLE IF NOT EXISTS de11tm.ykir_stg_delete_dim_cards(
+     max_update_dt
+          timestamp(0)
+);
+
+DROP TABLE IF EXISTS de11tm.ykir_stg_delete_dim_clients;
+CREATE TABLE IF NOT EXISTS de11tm.ykir_stg_delete_dim_clients(
+     max_update_dt
+          timestamp(0)
+);
+
+DROP TABLE IF EXISTS de11tm.ykir_stg_delete_dim_clients;
+CREATE TABLE IF NOT EXISTS de11tm.ykir_stg_delete_dim_clients(
+     max_update_dt
+          timestamp(0)
+);
+
+DROP TABLE IF EXISTS de11tm.ykir_stg_delete_dim_terminals;
+CREATE TABLE IF NOT EXISTS de11tm.ykir_stg_delete_dim_terminals(
+     max_update_dt
+          timestamp(0)
+);
+
+DROP TABLE IF EXISTS de11tm.ykir_stg_delete_fact_passport_blacklist;
+CREATE TABLE IF NOT EXISTS de11tm.ykir_stg_delete_fact_passport_blacklist(
+     max_update_dt
+          timestamp(0)
+);
+
+DROP TABLE IF EXISTS de11tm.ykir_stg_delete_fact_transactions;
+CREATE TABLE IF NOT EXISTS de11tm.ykir_stg_delete_fact_transactions(
+     max_update_dt
+          timestamp(0)
+);
+
+
+
+-- 5. Создаём таблицу отчёта:
+DROP TABLE IF EXISTS de11tm.ykir_rep_fraud;
+CREATE TABLE IF NOT EXISTS de11tm.ykir_rep_fraud(
+     event_dt timestamp(0),
+     passport varchar(15),
+     fio varchar(60),
+     phone varchar(16),
+     event_type smallint,
+     report_dt date
 );
